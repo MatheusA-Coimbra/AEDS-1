f63f41c061397b74bc44fefbb413da9ff6b3b63b
Matheus Arantes Coimbra <matheuscoimbra.arantes@gmail.com>
2024-08-28 16:05:14 -0300

    start of (exercicio00_01) v0.0 - Identification

---
+++ Guias/ed_00/Exercicios/exercicio00_01.c
@@ -0,0 +1,39 @@
+/**
+    Exercicio00 - v0.0 ___________
+    Author:Matheus Arantes Coimbra
+
+    Objetivos:
+    1. Montar um programa com opções para testar métodos -------------------------------------- (__)
+    2. Colocar em métodos todos os 50 primeiros exemplos de programas no Anexo C 02 ----------- (__)
+    3. Para cada método acrescentar uma opção correspondente para teste no programa principal - (__)
+    4. Testar todos os métodos e anotar suas saídas ao final (como comentários) --------------- (__)
+ */
+// dependencias
+#include <stdio.h>  // para entradas e saidas
+#include <stdlib.h> // para outras funcoes de uso geral
+
+int main() {
+    // definir dados
+
+    // identificar
+
+    // acoes
+
+    // encerar
+
+    return 0;
+}   // end main()
+
+/*
+------------------------------- documentação complementar
+------------------------------- notas / observacoes / comentarios
+------------------------------- previsao de testes
+
+------------------------------- historico
+    Versao      Data            Modificacao
+    0.0         23/08           esboco
+
+------------------------------- testes
+    Versao      Teste
+    0.0         00.(OK)         identificacao de programa
+*/
